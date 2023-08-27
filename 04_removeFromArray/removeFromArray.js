f24aa7bbb5dd9834e2c17b8464a9738effcbaebc
Luke Sullivan <sully097@gmail.com>
2023-08-27 15:08:33 +0100

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    // Create a new array to store the result.
+    const newArray = [...array];
 
+    // Remove desired elements.
+    for (const value of values) {
+        for (let i = newArray.length - 1; i >= 0; i--) {
+            if (value === newArray[i]) {
+                newArray.splice(i, 1);
+            }
+        }
+    }
+    
+    return newArray;
 };
+
+result = removeFromArray([1, 2, 3, 4], 1, 2);
+
+console.log(result);
 
 // Do not edit below this line
 module.exports = removeFromArray;
