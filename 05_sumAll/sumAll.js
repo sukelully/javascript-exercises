e2e724855aa893769215e6820c976e83b3fdc723
Luke Sullivan <sully097@gmail.com>
2023-09-11 16:04:56 +0100

    Pass 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof(a) !== "number" || typeof(b) !== "number") return "ERROR";
+    if (a < 0 || b < 0) return "ERROR";
 
+    const array = [a, b].sort(function(a, b) {
+        return a - b
+    });
+    const low = array[0];
+    const high = array[1];
+    
+    let sum = 0;
+    for (let i = high; i >= low; i--) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
